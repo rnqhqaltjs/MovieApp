67a1185056693fb1cc1e3a1e2bbe94474bec9d2e
김승우 <rnqhqaltjs@naver.com>
2025-02-13 23:22:29 +0900

    #15 TimeWheelPicker 구현

--- feature/setup/src/main/java/com/same/alarm/setup/SetupRoute.kt
+++ feature/setup/src/main/java/com/same/alarm/setup/SetupRoute.kt
@@ -1,6 +1,9 @@
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import com.same.alarm.setup.SetupScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun SetupRoute(
 ) {
--- feature/setup/src/main/java/com/same/alarm/setup/SetupScreen.kt
+++ feature/setup/src/main/java/com/same/alarm/setup/SetupScreen.kt
@@ -1,46 +1,41 @@
 package com.same.alarm.setup
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.material3.TimeInput
-import androidx.compose.material3.TimePicker
-import androidx.compose.material3.TimePickerState
-import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import java.util.Calendar
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.same.alarm.setup.component.TimeWheelPicker
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
-@OptIn(ExperimentalMaterial3Api::class)
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun SetupScreen () {
-    var selectedTime: TimePickerState? by remember { mutableStateOf(null) }
+    var selectedTime by remember { mutableStateOf(LocalTime.now()) }
 
-    DialUseStateExample(
-        onDismiss = {
-//            showDialExample = false
-        },
-        onConfirm = {
-                time ->
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(
+            text = "선택한 시간: ${selectedTime.format(DateTimeFormatter.ofPattern("hh:mm a"))}",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        TimeWheelPicker { time ->
             selectedTime = time
-//            showDialExample = false
-        },
-    )
-
-    if (selectedTime != null) {
-        val cal = Calendar.getInstance()
-        cal.set(Calendar.HOUR_OF_DAY, selectedTime!!.hour)
-        cal.set(Calendar.MINUTE, selectedTime!!.minute)
-        cal.isLenient = false
-//        Text("Selected time = ${formatter.format(cal.time)}")
-    } else {
-        Text("No time selected.")
+        }
     }
 }
 
@@ -52,57 +47,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DialUseStateExample(
-    onConfirm: (TimePickerState) -> Unit,
-    onDismiss: () -> Unit,
-) {
-    val currentTime = Calendar.getInstance()
 
-    val timePickerState = rememberTimePickerState(
-        initialHour = currentTime.get(Calendar.HOUR_OF_DAY),
-        initialMinute = currentTime.get(Calendar.MINUTE),
-        is24Hour = false,
-    )
 
-    Column {
-        TimeInput(
-            state = timePickerState,
-        )
-        Button(onClick = onDismiss) {
-            Text("Dismiss picker")
-        }
-        Button(onClick = { onConfirm(timePickerState) }) {
-            Text("Confirm selection")
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    var selectedTime: TimePickerState? by remember { mutableStateOf(null) }
-
-    DialUseStateExample(
-        onDismiss = {
-//            showDialExample = false
-        },
-        onConfirm = {
-                time ->
-            selectedTime = time
-//            showDialExample = false
-        },
-    )
-
-    if (selectedTime != null) {
-        val cal = Calendar.getInstance()
-        cal.set(Calendar.HOUR_OF_DAY, selectedTime!!.hour)
-        cal.set(Calendar.MINUTE, selectedTime!!.minute)
-        cal.isLenient = false
-//        Text("Selected time = ${formatter.format(cal.time)}")
-    } else {
-        Text("No time selected.")
-    }
 }
